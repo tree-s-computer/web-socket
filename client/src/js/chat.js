b5f06ec3103378c7324e4578daa260f0d07b2632
tree <hhyjs2r@gmail.com>
2024-03-19 11:28:23 +0900

    feat: login socket test

--- client/src/js/chat.js
+++ client/src/js/chat.js
@@ -1,25 +1,49 @@
 "use strict";
 
 const socket = io();
-
 const nickname = document.querySelector("#nickname");
 const chatList = document.querySelector(".chatting-list");
 const chatInput = document.querySelector(".chatting-input");
 const sendButton = document.querySelector(".send-button");
+const form = document.querySelector("form");
+
+const loginInfo = {
+  name: 123456789,
+  userid: "ungmo2@gmail.com",
+};
 
 const createChatList = (data) => {
+  const { name, msg } = data;
   const li = document.createElement("li");
-  li.innerText = data.name + "님이 - " + data.msg;
+  li.innerText = name + "님이 - " + msg;
   chatList.appendChild(li);
 };
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msgForm = document.getElementById("msgForm");
+
+  // 서버로 메시지를 전송한다.
+  socket.emit("chat", { msg: msgForm.value });
+  msgForm.value = "";
+});
 
 sendButton.addEventListener("click", () => {
   //like fetch :  body
   const prams = { name: nickname.value, msg: chatInput.value };
+  // 접속된 모든 클라이언트에게 메세지를 전송한다.
   socket.emit("chatting", prams);
 });
 
 socket.on("chatting", (data) => {
   //like fetch : get
   createChatList(data);
+});
+
+socket.emit("login", loginInfo); //1
+
+socket.on("login", (data) => {
+  //4
+  //like fetch : get
+  chatList.innerText = data + "님이 로그인 했어요. ";
 });
--- server/index.js
+++ server/index.js
@@ -15,9 +15,49 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("연결이 이루어졌습니다.");
 
+  // 접속한 클라이언트의 정보가 수신되면
   socket.on("chatting", (data) => {
-    console.log(data);
     io.emit("chatting", data);
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+  });
+
+  socket.on("login", (data) => {
+    //2
+    const { name, userid } = data;
+    console.log("Client logged-in:\n name:" + name + "\n userid: " + userid);
+
+    // socket에 클라이언트 정보를 저장한다
+    socket.name = name;
+    socket.userid = userid;
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    io.emit("login", name); //3
+  });
+
+  // 클라이언트로부터의 메시지가 수신되면
+  socket.on("chat", (data) => {
+    //data : 수신된 메세지 내용
+    console.log("Message from %s: %s", socket.name, data.msg);
+
+    const msg = {
+      from: {
+        name: socket.name,
+        userid: socket.userid,
+      },
+      msg: data.msg,
+    };
+
+    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+    socket.broadcast.emit("chat", msg);
+
+    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+    // socket.emit('s2c chat', msg);
+
+    // 접속된 모든 클라이언트에게 메시지를 전송한다
+    // io.emit('s2c chat', msg);
+
+    // 특정 클라이언트에게만 메시지를 전송한다
+    // io.to(id).emit('s2c chat', data);
   });
 });
 
